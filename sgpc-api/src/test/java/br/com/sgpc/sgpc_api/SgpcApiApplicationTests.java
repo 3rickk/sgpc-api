f930d47ba4d6772a7a4e8dc8c1d2e64f744fd0fa
ErickDEV <pedr03rick@gmail.com>
2025-05-29 02:45:12 -0300

    Inicializa projeto Spring Boot para o backend

---
+++ sgpc-api/src/test/java/br/com/sgpc/sgpc_api/SgpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgpc.sgpc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

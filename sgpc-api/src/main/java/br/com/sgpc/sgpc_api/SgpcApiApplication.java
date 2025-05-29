5346338a468687ea9f79d80a47d35470a9f39939
ErickDEV <pedr03rick@gmail.com>
2025-05-29 06:23:32 -0300

    Controller e App principal com hello rodando em um banco de dados local (h2 database) para teste

--- sgpc-api/src/main/java/br/com/sgpc/sgpc_api/SgpcApiApplication.java
+++ sgpc-api/src/main/java/br/com/sgpc/sgpc_api/SgpcApiApplication.java
@@ -2,12 +2,23 @@ package br.com.sgpc.sgpc_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SgpcApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SgpcApiApplication.class, args);
+    }
+}
 
-	public static void main(String[] args) {
-		SpringApplication.run(SgpcApiApplication.class, args);
-	}
+@RestController
+@RequestMapping("/api")
+class HelloWorldController {
 
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, SGPC!";
+    }
 }
---
+++ sgpc-api/src/main/java/br/com/sgpc/sgpc_api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.sgpc.sgpc_api.controller;// caminho do pacote
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping; // para rotas GET
+import org.springframework.web.bind.annotation.RestController; // para dizer que é um controller
+
+@RestController // indica que é um controller que retorna JSON (API REST)
+public class HelloController {
+
+    @GetMapping("/hello") // quando acessar http://localhost:8080/hello
+    public String hello() {
+        return "Olá, Erick! O backend está funcionando! 🚀";
+    }
+}

4bd83d77d3de121407d334f3abecdb4c8900a2ac
ErickDEV <pedr03rick@gmail.com>
2025-05-30 13:38:10 -0300

    docker-compose.yml (instalem o docker desktop e deixem abero antes de executar "docker compose up -d --build")

--- sgpc-api/src/main/java/br/com/sgpc/sgpc_api/controller/HelloController.java
+++ sgpc-api/src/main/java/br/com/sgpc/sgpc_api/controller/HelloController.java
@@ -1,7 +1,5 @@
 package br.com.sgpc.sgpc_api.controller;// caminho do pacote
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping; // para rotas GET
 import org.springframework.web.bind.annotation.RestController; // para dizer que é um controller
 

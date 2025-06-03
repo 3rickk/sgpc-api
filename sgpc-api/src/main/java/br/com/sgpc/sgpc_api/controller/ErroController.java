1a3bc4101b9b471239fc631923b0568c43825f7c
ErickDEV <pedr03rick@gmail.com>
2025-06-03 00:38:07 -0300

    Controle de erro/ Validação de Requisições

---
+++ sgpc-api/src/main/java/br/com/sgpc/sgpc_api/controller/ErroController.java
@@ -0,0 +1,39 @@
+package br.com.sgpc.sgpc_api.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ErroController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        String mensagem;
+
+        if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            mensagem = "Rota não encontrada";
+        } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            mensagem = "Erro interno do servidor";
+        } else {
+            mensagem = "Ocorreu um erro inesperado";
+        }
+
+        errorResponse.put("status", statusCode);
+        errorResponse.put("mensagem", mensagem);
+        errorResponse.put("path", request.getAttribute("javax.servlet.error.request_uri"));
+
+        return ResponseEntity
+            .status(statusCode)
+            .body(errorResponse);
+    }
+} 
